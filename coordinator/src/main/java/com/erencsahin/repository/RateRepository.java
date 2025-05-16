dad1da9ce43acacdfbe9db5a50db26dc32a83090
Eren Can Şahin <eren.sahin10@ogr.sakarya.edu.tr>
2025-05-17 02:30:55 +0300

    kafka-consumer module mostly done.

--- coordinator/src/main/java/com/erencsahin/repository/RateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.erencsahin.repository;
-
-import com.erencsahin.kafka.RateEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RateRepository extends JpaRepository<RateEntity, Long> {
-}
